4f137226bb8ec6d5c08e60e62f721eedfbd41877
kun <i@imux.top>
2021-04-09 15:02:09 +0800

    update

--- src/mat4x4.cpp
+++ src/mat4x4.cpp
@@ -24,12 +24,14 @@ int16_t* matrix_mul_neon(const int16_t *aa,
     int16x4_t _bb;
     int16x4_t _cc;
     int16_t* cc = (int16_t*) malloc(sizeof(int16_t)*dim*dim);
+    int16_t* c = (int16_t*) malloc(sizeof(int16_t)*dim);
     for (int i = 0; i < dim; i++)
     {
         _aa = vld1_s16((int16_t*)aa[i*dim]);
         _bb = vld1_s16((int16_t*)bb[i*dim]);
         _cc = vmul_s16(_aa, _bb);
-        vst1_s16((int16_t*)cc[i*dim], _cc);
+        vst1_s16(c, _cc);
+        memcpy(c, cc+i*dim, sizeof(int16_t)*dim);
     }
     return cc;
 }
