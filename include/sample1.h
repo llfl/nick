f002dd9979eab3e092f22893a3ac1e26674c8d28
kun <i@imux.top>
2021-04-09 09:25:34 +0800

    Update sample1.h

--- include/sample1.h
+++ include/sample1.h
@@ -32,10 +32,15 @@
 #ifndef GTEST_SAMPLES_SAMPLE1_H_
 #define GTEST_SAMPLES_SAMPLE1_H_
 
+#define false 0
+#define true 1
+typedef int bool;
+
 // Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
 int Factorial(int n);
 
 // Returns true if and only if n is a prime number.
 bool IsPrime(int n);
+
 
 #endif  // GTEST_SAMPLES_SAMPLE1_H_
