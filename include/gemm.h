953b897fffe5fb2c1a752c35fffcee7265c50239
kun <i@imux.top>
2021-04-09 22:40:03 +0800

    update

--- include/gemm.h
+++ include/gemm.h
@@ -17,13 +17,16 @@ T* gemm(
     T* out = (T*)malloc(sizeof(T)*Ashape[H_INDEX]*Bshape[W_INDEX]);
     memset(out,0,sizeof(T)*Ashape[H_INDEX]*Bshape[W_INDEX]);
 
-    for(int i = 0; i < Ashape[H_INDEX]; i += 4){
-        for(int j = 0; j < Bshape[W_INDEX]; j += 4){
-            for(int k=0; k < Bshape[H_INDEX]; k += 4){
+    int M = Ashape[H_INDEX] / 4;
+    int N = Bshape[W_INDEX] / 4;
+    int K = Bshape[H_INDEX] / 4;
+    for(int i = 0; i < M; i++){
+        for(int j = 0; j < N; j++){
+            for(int k=0; k < K; k++){
                 mm4x4<T>(
-                    (T*)(A+i*Bshape[H_INDEX]/4+k),
-                    (T*)(B+k*Bshape[H_INDEX]/4+j),
-                    (T*)(out+i*Bshape[H_INDEX]/4+j));
+                    (T*)(A+(i*K+k)*4),
+                    (T*)(B+(k*N+j)*4),
+                    (T*)(out+(i*N+j)*4));
             }
         }
     }
