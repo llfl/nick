e083e026a6fb46b6d99fc1f9ff75cdfb4351fd20
kun <i@imux.top>
2021-04-10 11:43:17 +0800

    update

--- include/gemm.h
+++ include/gemm.h
@@ -20,10 +20,19 @@ T* gemm(
     for(int i = 0; i < Ashape[H_INDEX]; i += 4){
         for(int j = 0; j < Bshape[W_INDEX]; j += 4){
             for(int k=0; k < Bshape[H_INDEX]; k += 4){
-                mm4x4<T>(
+                mm4x4s<T>(
                     (T*)(A+i*Bshape[H_INDEX]+k),
+                    (T*)(A+(i+1)*Bshape[H_INDEX]+k),
+                    (T*)(A+(i+2)*Bshape[H_INDEX]+k),
+                    (T*)(A+(i+3)*Bshape[H_INDEX]+k),
                     (T*)(B+k*Bshape[W_INDEX]+j),
-                    (T*)(out+i*Bshape[W_INDEX]+j));
+                    (T*)(B+(k+1)*Bshape[W_INDEX]+j),
+                    (T*)(B+(k+2)*Bshape[W_INDEX]+j),
+                    (T*)(B+(k+3)*Bshape[W_INDEX]+j),
+                    (T*)(out+i*Bshape[W_INDEX]+j),
+                    (T*)(out+(i+1)*Bshape[W_INDEX]+j),
+                    (T*)(out+(i+2)*Bshape[W_INDEX]+j),
+                    (T*)(out+(i+3)*Bshape[W_INDEX]+j),);
             }
         }
     }
