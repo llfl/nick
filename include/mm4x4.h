546044a23db84f83e8edf63a28dbac46b5995ee6
kun <i@imux.top>
2021-04-10 11:03:22 +0800

    update

--- include/mm4x4.h
+++ include/mm4x4.h
@@ -8,12 +8,13 @@ template <typename T>
 void mm4x4(
     const T *A,
     const T *B,
-          T *C
+          T *C,
+        int offset
 ){
     for (int i = 0; i < 4; i++){
         for (int j = 0; j < 4; j++){
             for (int k = 0; k < 4; k++){
-                C[i*4+j] += A[i*4+k] * B[k*4+j];
+                C[i*4+j+offset] += A[i*4+k+offset] * B[k*4+j+offset];
             }
         }
     }
